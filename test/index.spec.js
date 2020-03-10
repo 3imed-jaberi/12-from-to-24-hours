1bee4bb705986acd75dca4bdf29924aaebd192d6
3imed-jaberi <imed_jebari@hotmail.fr>
2020-03-10 19:59:32 +0100

    update test using `mocha ☕️` and `chai 🍵` 👻 --npm ..

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,16 +1,16 @@
 const { expect } = require('chai');
 const { __resolve, __reject } = require('./_data.json');
-const package = require('..'); 
+const Switch12FromTo24Hours = require('..'); 
 
 
 describe('Test using `mocha ☕️` and `chai 🍵` 👻', () => {
 
   it ('Check All Success Resulat ✔️', () => {
-    __resolve.map(({ _24Time, _12Time }) => { expect(package(_24Time)).to.equal(_12Time) });
+    __resolve.map(({ _24Time, _12Time }) => { expect(Switch12FromTo24Hours(_24Time)).to.equal(_12Time) });
   });
 
   it ('Check All Failed Resulat ❌', () => {
-    __reject.map(({ _24Time, _12Time }) => { expect(() => package(_24Time)).to.throw(new Error(_12Time).message) });
+    __reject.map(({ _24Time, _12Time }) => { expect(() => Switch12FromTo24Hours(_24Time)).to.throw(new Error(_12Time).message) });
   });
   
 });
