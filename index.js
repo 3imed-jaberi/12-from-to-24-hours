1d7f1fd4df5682a52e44f893a1ad81ef6b48c898
3imed-jaberi <imed_jebari@hotmail.fr>
2020-03-10 18:56:48 +0100

    add from 12 to 24 hours logic 🔖 ..

--- index.js
+++ index.js
@@ -11,15 +11,19 @@
 
 
 module.exports = (time) => {
-  
-	let hours, minutes, suffix;
+	let hours, minutes, suffix, __AM_RESULT__, __PM_RESULT__;
 
-  hours = +time.slice(0, 2), // time.indexOf(':')
-  minutes = time.slice(-2), // -hours.toString().length
-  suffix = hours >= 12 ? 'PM' : 'AM';
-  
-  hours =	hours > 12 ? hours - 12 : (hours === 0 ? 12 : hours);
-  
-  
-  return `${hours}:${minutes} ${suffix}`;
+		// hours = +time.slice(0, 2), // time.indexOf(':')
+		// minutes = time.slice(-2), // -hours.toString().length
+		// suffix = hours >= 12 ? 'PM' : 'AM';
+		
+    // hours =	hours > 12 ? hours - 12 : (hours === 0 ? 12 : hours);
+    
+    hours = +time.slice(0, 2),
+		minutes = time.slice(3, 5),
+		suffix = time.slice(-2),
+		__AM_RESULT__ = `${hours === 12 ? '00' : hours}:${minutes}`,
+		__PM_RESULT__ = `${hours + 12 === 24 ? hours : hours + 12}:${minutes}`;
+
+		return suffix === 'AM' ? __AM_RESULT__ : __PM_RESULT__ ;
 };
