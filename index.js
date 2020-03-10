58c866286e053e098996fd1f55973a4d3d3fc9c3
3imed-jaberi <imed_jebari@hotmail.fr>
2020-03-10 18:51:48 +0100

    add my logic plan 📋 ..

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// ____ init 🚧 ____ 
+//
+// SWITCH BETWEEN 24 AND 12 HOURS MADE EASY .. 
+//
+//
+// TODO: 
+//  - Think of the version that supports all versions of Node.js and a version with EcmaScript features ..
+//  - Check the input for validate this pattern: xx:xx (x: number)
+//  - Write Logic for 24 hours to 12 hours .. 
+//  - Write Logic for 12 hours to 24 hours ..
+//
