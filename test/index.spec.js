0e09e10c8d67f41c82d247539a86658f027e11fb
3imed-jaberi <imed_jebari@hotmail.fr>
2020-03-10 19:17:25 +0100

    add test using `mocha ☕️` and `chai 🍵` 👻 ..

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,1 +1,16 @@
-// ____ init 🚧 ____ 
+const { expect } = require('chai');
+const { __resolve, __reject } = require('./_data.json');
+const package = require('..'); 
+
+
+describe('Test using `mocha ☕️` and `chai 🍵` 👻', () => {
+
+  it ('Check All Success Resulat ✔️', () => {
+    __resolve.map(({ _24Time, _12Time }) => { expect(package(_24Time)).to.equal(_12Time) });
+  });
+
+  it ('Check All Failed Resulat ❌', () => {
+    __reject.map(({ _24Time, _12Time }) => { expect(() => package(_24Time)).to.throw(new Error(_12Time).message) });
+  });
+  
+});
